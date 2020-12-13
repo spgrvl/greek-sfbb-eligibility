6a8301fe5b8a8b83944ae4ad32e7c084de370926
Spyros <spgrvl@gmail.com>
2020-12-13 17:26:58 +0200

    Accounted for some more cases where SFBB services are available in given address and a url for further actions is provided.

--- eligibility_check.py
+++ eligibility_check.py
@@ -11,14 +11,26 @@ elif os.name == "posix":
 else:
     print("Error: Chrome path is not set for this os: " + os.name)
 
-def print_error():
-    error_message = driver.find_element_by_css_selector("div[class='alert alert-danger']").text
-    print(error_message)
+def check_message():
+    try:
+        active_msg = driver.find_element_by_id("ctl00_cphMain_lblSuccessMsgActive")
+        print(active_msg.text)
+        coupon_url = active_msg.find_element_by_tag_name('a').get_attribute('href')
+        print(coupon_url)
+    except:
+        try:
+            inactive_msg = driver.find_element_by_id("ctl00_cphMain_lblSuccessMsgInActive")
+            print(inactive_msg.text)
+            signup_url = inactive_msg.find_element_by_tag_name('a').get_attribute('href')
+            print(signup_url)
+        except:
+            unavailable_msg = driver.find_element_by_css_selector("div[class='alert alert-danger']")
+            print(unavailable_msg.text)
     driver.quit()
     exit()
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument('--headless')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 
@@ -33,7 +45,7 @@ time.sleep(1)
 try:
     driver.find_element_by_id("ctl00_cphMain_txtAddress_I").send_keys(config.address)
 except:
-    print_error()
+    check_message()
 
 driver.find_element_by_id("ctl00_cphMain_btnCheckAddress").click()
 time.sleep(1)
@@ -41,8 +53,8 @@ time.sleep(1)
 try:
     driver.find_element_by_id("ctl00_cphMain_txtStreetNumber_I").send_keys(config.home_number)
 except:
-    print_error()
+    check_message()
 
 driver.find_element_by_id("ctl00_cphMain_btnCheckEligibility").click()
 time.sleep(1)
-print_error()
+check_message()
